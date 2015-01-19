d3bed31dd0dd380f668eca785e3b4c7db63bccb1
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-01-19 20:39:41 +0100

    lowercase field names, show invalid entries

--- scripts/_bibtex.py
+++ scripts/_bibtex.py
@@ -7,8 +7,9 @@ import mmap
 import contextlib
 from pprint import pprint
 
-from pybtex.database.input.bibtex import BibTeXEntryIterator
+from pybtex.database.input.bibtex import BibTeXEntryIterator, Parser
 from pybtex.scanner import PybtexSyntaxError
+from pybtex.exceptions import PybtexError
 from pybtex.textutils import normalize_whitespace
 
 __all__ = ['load']
@@ -29,7 +30,7 @@ def load(filename, encoding=None):
 def iterentries(source):
     try:
         for entrytype, (bibkey, fields) in BibTeXEntryIterator(source):
-            fields = {name: normalize_whitespace(''.join(values))
+            fields = {name.lower(): normalize_whitespace(''.join(values))
                 for name, values in fields}
             yield bibkey, (entrytype, fields)
     except PybtexSyntaxError as e:
@@ -39,9 +40,28 @@ def iterentries(source):
         raise
 
 
+def check(filename):
+    parser = CheckParser()
+    parser.parse_file(filename)
+    return parser.error_count
+    
+
+class CheckParser(Parser):
+    def __init__(self, *args, **kwargs):
+        super(CheckParser, self).__init__(*args, **kwargs)
+        self.error_count = 0
+    def process_entry(self, *args, **kwargs):
+        try:
+            super(CheckParser, self).process_entry(*args, **kwargs)
+        except PybtexError as e:
+            print e
+            self.error_count +=1
+
+
 if __name__ == '__main__':    
     for filename in glob.glob('../references/bibtex/*.bib'):
         print(filename)
         entries = load(filename)
         print(len(entries))
-        pprint(next(entries.iteritems()))
+        #pprint(next(entries.iteritems()))
+        print('%d invalid' % check(filename))
