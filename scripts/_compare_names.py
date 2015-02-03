60e0c7f46cbf8db86207059bb33f84d8981f1191
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-02-03 22:29:18 +0100

    check editor field, stream entries

--- scripts/_compare_names.py
+++ scripts/_compare_names.py
@@ -6,24 +6,26 @@ import bib
 import _bibtex
 
 
-def pauthor1(s):
+def names1(s):
     return [(n.get('firstname', ''), n.get('lastname', ''), n.get('jr', ''))
         for n in bib.pauthor(s)]
 
 
-def pauthor2(s):
+def names2(s):
     return [(first, ' '.join(n for n in (prelast, last) if n), lineage)
         for prelast, last, first, lineage in _bibtex.names(s)]
 
 
 for filename in glob.glob('../references/bibtex/*.bib'):
-    entries = _bibtex.load(filename)
-    for bibkey, (entrytype, fields) in entries.iteritems():
-        author = fields.get('author', '')
-        a1, a2 = pauthor1(author), pauthor2(author)
-        if a1 != a2:
-            print filename, bibkey
-            print repr(author)
-            print a1
-            print a2
-            print
+    with _bibtex.memorymapped(filename) as m:
+        print filename.center(80, '#')
+        for bibkey, (entrytype, fields) in _bibtex.iterentries(m):
+            for role in ('author', 'editor'):
+                names = fields.get(role, '')
+                n1, n2 = names1(names), names2(names)
+                if n1 != n2:
+                    print filename, bibkey, role
+                    print repr(names)
+                    print n1
+                    print n2
+                    print
