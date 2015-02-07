5782e996a910df56aa1c7e54f44bc239eea44dde
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-02-07 16:04:59 +0100

    examine file-priority based merging

--- scripts/_bibfiles.py
+++ scripts/_bibfiles.py
@@ -55,6 +55,10 @@ class Collection(list):
         result = Database(filename)
         db = result.connect()
 
+        db.execute('CREATE TABLE file ('
+            'name TEXT NOT NULL, '
+            'priority INTEGER NOT NULL, '
+            'PRIMARY KEY (name))')
         db.execute('CREATE TABLE entry ('
             'filename TEXT NOT NULL, '
             'bibkey TEXT NOT NULL, '
@@ -62,13 +66,16 @@ class Collection(list):
             'hash TEXT, '
             'fields TEXT NOT NULL, '
             'title TEXT, '
-            'PRIMARY KEY (filename, bibkey))')
+            'PRIMARY KEY (filename, bibkey), '
+            'FOREIGN KEY(filename) REFERENCES file(name))')
 
         db.execute('PRAGMA synchronous = OFF')
         db.execute('PRAGMA journal_mode = MEMORY')
 
         for b in self:
             print(b.filepath)
+            db.execute('INSERT INTO file (name, priority) VALUES (?, ?)',
+                (b.filename, b.priority))  
             db.executemany('INSERT INTO entry '
                 '(filename, bibkey, entrytype, fields, title) VALUES (?, ?, ?, ?, ?)',
                 ((b.filename, bibkey, entrytype, json.dumps(fields), fields.get('title'))
@@ -92,8 +99,9 @@ class Collection(list):
                 ((bib.keyid(json.loads(fields), words), filename, bibkey)
                 for filename, bibkey, fields in rows))
             db.commit()
-        db.execute('CREATE INDEX IF NOT EXISTS ix_hash ON entry(hash)')
+        db.execute('CREATE INDEX ix_hash ON entry(hash)')
         print('%d keyids' % db.execute('SELECT count(hash) FROM entry').fetchone())
+        print('%d distinct keyids' % db.execute('SELECT count(DISTINCT hash) FROM entry').fetchone())
         db.close()
         return result
 
@@ -140,8 +148,13 @@ class Database(object):
 
     def _iter(self):
         with contextlib.closing(self.connect()) as db:
+            nopriority, = db.execute('SELECT EXISTS (SELECT 1 FROM entry '
+                'WHERE NOT EXISTS (SELECT 1 FROM file '
+                'WHERE name = filename))').fetchone()
+            assert not nopriority
             result = db.execute('SELECT hash, filename, bibkey, entrytype, fields '
-                'FROM entry ORDER BY hash, filename, bibkey')
+                'FROM entry JOIN file ON filename = name '
+                'ORDER BY hash, priority DESC, filename, bibkey')
             while True:
                 rows = result.fetchmany(100)
                 if not rows:
@@ -154,7 +167,50 @@ class Database(object):
             yield hs, [(fn, bk, et, fs) for hs, fn, bk, et, fs in group]
 
 
+def _test_merge():
+    import sqlalchemy as sa
+    from itertools import chain
+
+    engine = sa.create_engine('postgresql://postgres@/overrides')
+    metadata = sa.MetaData()
+    overrides = sa.Table('overrides', metadata,
+        sa.Column('hash', sa.Text, primary_key=True),
+        sa.Column('field', sa.Text, primary_key=True),
+        sa.Column('file1', sa.Text, primary_key=True),
+        sa.Column('bibkey1', sa.Text, primary_key=True),
+        sa.Column('file2', sa.Text, primary_key=True),
+        sa.Column('bibkey2', sa.Text, primary_key=True),
+        sa.Column('value1', sa.Text),
+        sa.Column('value2', sa.Text))
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
+    insert_ov = overrides.insert(bind=engine).execute
+
+    UNION = {'lgcode', 'fn', 'asjp_name', 'hhtype', 'isbn'}
+    ET = type('entrytype', (object,), {'__str__': lambda self: self.__class__.__name__})()
+
+    with contextlib.closing(Database().connect()) as conn, conn as conn:
+        conn.executemany('UPDATE file SET priority = ? WHERE name = ?',
+            ((b.priority, b.filename) for b in Collection()))
+
+    for hash, grp in Database():
+        merged, overrider = {}, {}
+        for filename, bibkey, entrytype, fields in grp:
+            for field, value in chain([(ET, entrytype)], fields.iteritems()):
+                if field in UNION:
+                    continue
+                elif field not in merged:
+                    merged[field] = value
+                    overrider[field] = (filename, bibkey)
+                elif value.lower() != merged[field].lower():
+                    ofilename, obibkey = overrider[field]
+                    insert_ov(hash=hash, field=str(field),
+                        file1=ofilename, bibkey1=obibkey, value1=merged[field],
+                        file2=filename, bibkey2=bibkey, value2=value)
+
+
 if __name__ == '__main__':
     c = Collection()
     c.to_sqlite()
     #c.roundtrip_all()
+    #_test_merge()
