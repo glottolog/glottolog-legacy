4e08bcc2a4812ef0392ed7b55b0b3a22c26d41d6
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-02-08 19:06:38 +0100

    use finer decomposition, allow for field-level priorities

--- scripts/_bibfiles.py
+++ scripts/_bibfiles.py
@@ -98,15 +98,25 @@ class Database(object):
             'name TEXT NOT NULL, '
             'priority INTEGER NOT NULL, '
             'PRIMARY KEY (name))')
+        db.execute('CREATE TABLE field ('
+            'filename TEXT NOT NULL, '
+            'field TEXT NOT NULL, '
+            'priority INTEGER NOT NULL, '
+            'PRIMARY KEY (filename, field), '
+            'FOREIGN KEY(filename) REFERENCES file(name))')
         db.execute('CREATE TABLE entry ('
             'filename TEXT NOT NULL, '
             'bibkey TEXT NOT NULL, '
-            'entrytype TEXT NOT NULL, '
             'hash TEXT, '
-            'fields TEXT NOT NULL, '
-            'title TEXT, '
             'PRIMARY KEY (filename, bibkey), '
             'FOREIGN KEY(filename) REFERENCES file(name))')
+        db.execute('CREATE TABLE value ('
+            'filename TEXT NOT NULL, '
+            'bibkey TEXT NOT NULL, '
+            'field TEXT NOT NULL, '
+            'value TEXT NOT NULL, '
+            'PRIMARY KEY (filename, bibkey, field), '
+            'FOREIGN KEY(filename, bibkey) REFERENCES entry(filename, bibkey))')
 
         db.execute('PRAGMA synchronous = OFF')
         db.execute('PRAGMA journal_mode = MEMORY')
@@ -114,30 +124,40 @@ class Database(object):
         for b in bibfiles:
             print(b.filepath)
             db.execute('INSERT INTO file (name, priority) VALUES (?, ?)',
-                (b.filename, b.priority))  
-            db.executemany('INSERT INTO entry '
-                '(filename, bibkey, entrytype, fields, title) VALUES (?, ?, ?, ?, ?)',
-                ((b.filename, bibkey, entrytype, json.dumps(fields), fields.get('title'))
-                for bibkey, (entrytype, fields) in b.iterentries()))
+                (b.filename, b.priority))
+            for bibkey, (entrytype, fields) in b.iterentries():
+                db.execute('INSERT INTO entry '
+                    '(filename, bibkey) VALUES (?, ?)',
+                    (b.filename, bibkey))
+                db.executemany('INSERT INTO value '
+                    '(filename, bibkey, field, value) VALUES (?, ?, ?, ?)',
+                    ((b.filename, bibkey, field, value) for field, value in
+                     itertools.chain([('ENTRYTYPE', entrytype)], fields.iteritems())))
             db.commit()
         print('\n'.join('%d %s' % (n, f) for f, n in db.execute(
             'SELECT filename, count(*) FROM entry GROUP BY filename')))
         print('%d entries' % db.execute('SELECT count(*) FROM entry').fetchone())
 
         words = collections.Counter()
-        for title, in db.execute('SELECT title FROM entry WHERE title IS NOT NULL'):
+        for title, in db.execute('SELECT value FROM value WHERE field = ?', ('title',)):
             words.update(bib.wrds(title))
         print('%d title words' % len(words))
 
-        cursor = db.execute('SELECT filename, bibkey, fields FROM entry')
-        while True:
-            rows = cursor.fetchmany(1000)
-            if not rows:
-                cursor.close()
-                break
-            db.executemany('UPDATE entry SET hash = ? WHERE filename = ? AND bibkey = ?',
-                ((bib.keyid(json.loads(fields), words), filename, bibkey)
-                for filename, bibkey, fields in rows))
+        for filename, in db.execute('SELECT name FROM file ORDER BY name'):
+            cursor = db.execute('SELECT bibkey FROM entry WHERE filename = ? '
+                'ORDER BY bibkey', (filename,))
+            while True:
+                bibkeys = cursor.fetchmany(1000)
+                if not bibkeys:
+                    cursor.close()
+                    break
+                (first,), (last,) = bibkeys[0], bibkeys[-1]
+                rows = db.execute('SELECT bibkey, field, value FROM value '
+                    'WHERE filename = ? AND bibkey BETWEEN ? AND ? '
+                    'ORDER BY bibkey', (filename, first, last))
+                db.executemany('UPDATE entry SET hash = ? WHERE filename = ? AND bibkey = ?',
+                    ((bib.keyid({k: v for b, k, v in grp}, words), filename, bibkey)
+                    for bibkey, grp in itertools.groupby(rows, operator.itemgetter(0))))
             db.commit()
         db.execute('CREATE INDEX ix_hash ON entry(hash)')
         print('%d keyids' % db.execute('SELECT count(hash) FROM entry').fetchone())
@@ -151,26 +171,42 @@ class Database(object):
     def connect(self):
         return sqlite3.connect(self.filename)
 
-    def _iter(self):
+    def _iter(self, chunksize=1000):
         with contextlib.closing(self.connect()) as db:
             nopriority, = db.execute('SELECT EXISTS (SELECT 1 FROM entry '
                 'WHERE NOT EXISTS (SELECT 1 FROM file '
                 'WHERE name = filename))').fetchone()
             assert not nopriority
-            cursor = db.execute('SELECT hash, filename, bibkey, entrytype, fields '
-                'FROM entry JOIN file ON filename = name '
-                'ORDER BY hash, priority DESC, filename, bibkey')
+            cursor = db.execute('SELECT e.hash, v.field, v.value, v.filename, v.bibkey '
+                'FROM value AS v '
+                'JOIN entry AS e ON v.filename = e.filename AND v.bibkey = e.bibkey '
+                'JOIN file AS f ON v.filename = f.name '
+                'LEFT JOIN field AS d ON v.filename = d.filename AND v.field = d.field '
+                'ORDER BY e.hash, v.field, coalesce(d.priority, f.priority) DESC, v.filename, v.bibkey')
             while True:
-                rows = cursor.fetchmany(1000)
+                rows = cursor.fetchmany(chunksize)
                 if not rows:
                     cursor.close()
                     return
-                for hs, fn, bk, et, fs in rows:
-                    yield hs, fn, bk, et, json.loads(fs)
+                for r in rows:
+                    yield r
 
-    def __iter__(self):
-        for hs, group in itertools.groupby(self._iter(), operator.itemgetter(0)):
-            yield hs, [(fn, bk, et, fs) for hs, fn, bk, et, fs in group]
+    def __iter__(self, get_hash=operator.itemgetter(0), get_field=operator.itemgetter(1)):
+        for hash, grp in itertools.groupby(self._iter(), get_hash):
+            yield (hash, [(field, [(vl, fn, bk) for hs, fd, vl, fn, bk in g])
+                for field, g in itertools.groupby(grp, get_field)])
+
+    def merged(self, union={'lgcode', 'fn', 'asjp_name', 'hhtype', 'isbn'}):
+        for hash, grp in self:
+            fields = {field: values[0][0] if field not in union
+                else ', '.join(vl for vl, fn, bk in values)
+                for field, values in grp}
+            fields['src'] = ', '.join(sorted(set(fn
+                for field, values in grp for vl, fn, bk in values)))
+            fields['srctrickle'] = ', '.join(sorted(set('%s#%s' % (fn, bk)
+                for field, values in grp for vl, fn, bk in values)))
+            entrytype = fields.pop('ENTRYTYPE')
+            yield hash, (entrytype, fields)
 
 
 def _test_merge():
@@ -192,27 +228,21 @@ def _test_merge():
     metadata.create_all(engine)
     insert_ov = overrides.insert(bind=engine).execute
 
-    UNION = {'lgcode', 'fn', 'asjp_name', 'hhtype', 'isbn'}
-    ET = type('entrytype', (object,), {'__str__': lambda self: self.__class__.__name__})()
-
     with contextlib.closing(Database().connect()) as conn, conn as conn:
         conn.executemany('UPDATE file SET priority = ? WHERE name = ?',
             ((b.priority, b.filename) for b in Collection()))
 
+    UNION = {'lgcode', 'fn', 'asjp_name', 'hhtype', 'isbn'}
+
     for hash, grp in Database():
-        merged, overrider = {}, {}
-        for filename, bibkey, entrytype, fields in grp:
-            for field, value in chain([(ET, entrytype)], fields.iteritems()):
-                if field in UNION:
-                    continue
-                elif field not in merged:
-                    merged[field] = value
-                    overrider[field] = (filename, bibkey)
-                elif value.lower() != merged[field].lower():
-                    ofilename, obibkey = overrider[field]
-                    insert_ov(hash=hash, field=str(field),
-                        file1=ofilename, bibkey1=obibkey, value1=merged[field],
-                        file2=filename, bibkey2=bibkey, value2=value)
+        for field, values in grp:
+            if field in UNION:
+                continue
+            value1, file1, bibkey1 = values[0]
+            for value2, file2, bibkey2 in values[1:]:
+                if value1.lower() != value2.lower():
+                    insert_ov(hash=hash, field=field, value1=value1, value2=value2,
+                        file1=file1, bibkey1=bibkey1, file2=file2, bibkey2=bibkey2)
 
 
 if __name__ == '__main__':
