d6d01d214a143dea3507fb6887fdcf66722ab8e4
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-03-31 17:42:57 +0200

    work on ref_id backtracking

--- scripts/_monsterold.py
+++ scripts/_monsterold.py
@@ -5,12 +5,15 @@ import re
 import csv
 import glob
 import mmap
+import urllib
+import zipfile
 import sqlite3
 import datetime
 import contextlib
 import collections
 
-FILES = 'monsteroldv?*.bib'
+BIBFILES = 'monsteroldv?*.bib'
+CSVFILES = 'monsteroldv?*.csv'
 DBFILE = '_monsterold.sqlite3'
 
 ENTRY = re.compile(r'''
@@ -26,18 +29,30 @@ FIELD = re.compile(r'''
 ''', re.VERBOSE | re.MULTILINE)
 
 
-def iterfiles(pattern=FILES):
+def sortedglob(pattern):
     def sortkey(filename):
         name, ext = os.path.splitext(filename)
         numeric = [int(x) if x.isdigit() else x for x in re.split('(\d+)', name) if x]
         return numeric, ext
-    
-    for bibfile in sorted(glob.glob(pattern), key=sortkey):
+    return sorted(glob.glob(pattern), key=sortkey)
+
+
+def iterfiles(pattern=BIBFILES, bibless_pattern=CSVFILES, include_bibless=False):
+    csvseen = set()
+    for bibfile in sortedglob(pattern):
         mtime = datetime.datetime.fromtimestamp(os.stat(bibfile).st_mtime)
         dirname, basename = os.path.split(bibfile)
         name, ext = os.path.splitext(basename)
         csvfile = os.path.join(dirname, '%s.csv' % name)
         yield bibfile, mtime, csvfile
+        csvseen.add(csvfile)
+    if not include_bibless:
+        return
+    for csvfile in sortedglob(bibless_pattern):
+        if csvfile in csvseen:
+            continue
+        mtime = datetime.datetime.fromtimestamp(os.stat(csvfile).st_mtime)
+        yield csvfile, mtime, csvfile
 
 
 def iterentries(filename, fieldcls=dict):
@@ -76,7 +91,7 @@ def showstats(filename, ntop=10):
     print '\n'.join('%d\t%s' % (n, f) for f, n in counts.most_common(ntop))
 
 
-def to_csv(bibfile, csvfile, sortkey=lambda (fn, bk, hs, id): (fn.lower(), bk.lower())):
+def bib_to_csv(bibfile, csvfile, sortkey=lambda (fn, bk, hs, id): (fn.lower(), bk.lower())):
     rows = sorted(iterrows(bibfile), key=sortkey)
     if not rows:
         print '...no matched entries, skipped'
@@ -115,9 +130,39 @@ def iterrows(bibfile):
         print '%d entries skipped' % skipped
 
 
+def to_csv():
+    if not os.path.exists('monsteroldv75.bib'):
+        fn, _ = urllib.urlretrieve('https://github.com/clld/glottolog-data/blob/41f2108121734c86e60fd51e42a8cbd60d61b2a0/references/monster.zip?raw=true',
+            'monsteroldv75.zip')
+        with zipfile.ZipFile(fn, 'r') as z:
+            m = next(i for i in z.infolist() if i.filename == 'monster.bib')
+            m.filename = 'monsteroldv75.bib'
+            z.extract(m)
+        
+    for bibfile, mtime, csvfile in iterfiles():
+        print bibfile, mtime
+        #showstats(bibfile)
+        bib_to_csv(bibfile, csvfile)
+
+
 def to_sqlite(filename=DBFILE):
+    if not os.path.exists('monsteroldv76.csv'):
+        urllib.urlretrieve('https://github.com/clld/glottolog-data/blob/ab6dcae9915834ba3940aa3225fd0a1cbfb16f0f/references/monster.csv?raw=true',
+            'monsteroldv76.csv')
+    
+    if not os.path.exists('monsteroldv77.csv'):
+        urllib.urlretrieve('https://github.com/clld/glottolog-data/blob/0e923c3c7dabd2b901ee295a7cfa526c20f6d6c3/references/monster.csv?raw=true'
+            'monsteroldv77.csv')
+
     if os.path.exists(filename):
         os.remove(filename)
+
+    def normalize_filename(filename):
+        filename = filename.lower()
+        if filename.endswith('.bib'):
+            filename = filename[:-4]
+        return filename
+
     with sqlite3.connect(filename) as conn:
         conn.execute('PRAGMA synchronous = OFF')
         conn.execute('PRAGMA journal_mode = MEMORY')
@@ -135,7 +180,7 @@ def to_sqlite(filename=DBFILE):
             'PRIMARY KEY (monster, filename, bibkey, monster), '
             'FOREIGN KEY (monster) REFERENCES monster(idx))')
         conn.execute('CREATE INDEX ix_id ON entry(id)')
-        for bibfile, mtime, csvfile in iterfiles():
+        for bibfile, mtime, csvfile in iterfiles(include_bibless=True):
             if not os.path.exists(csvfile):
                 continue
             print csvfile
@@ -144,7 +189,8 @@ def to_sqlite(filename=DBFILE):
             try:
                 conn.executemany('INSERT INTO entry (monster, filename, bibkey, hash, id) '
                     'VALUES (?, ?, ?, ?, ?)',
-                    ((rowid,) + tuple(row) for row in from_csv(csvfile)))
+                    ((rowid, normalize_filename(filename), bibkey, hash, id)
+                    for filename, bibkey, hash, id in from_csv(csvfile)))
             except sqlite3.IntegrityError:
                 print '...uniqueness problem, skipped'
                 conn.rollback()
@@ -153,10 +199,7 @@ def to_sqlite(filename=DBFILE):
 
 
 def build():
-    for bibfile, mtime, csvfile in iterfiles():
-        print bibfile, mtime
-        #showstats(bibfile)
-        to_csv(bibfile, csvfile)
+    to_csv()
     to_sqlite()
 
 
